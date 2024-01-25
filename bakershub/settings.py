6d1cd95ebf8b8c327ceb62e54cf902ee2deef44b
amy-1989 <>
2024-01-25 21:12:45 +0000

    add deployment settings

--- bakershub/settings.py
+++ bakershub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*z3*-gf02%%^sjbsu4kw*m74q&+z50-g2#jbr#1^uy6r#%#ia'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
