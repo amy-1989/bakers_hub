b0d368c16d05e7c814eee32a7373fa196adf8fd2
amy-1989 <>
2024-01-30 21:27:43 +0000

    add comment, review and category models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 from .models import Post
+from .models import Comment
+from .models import Reviews
+from .models import Category
 
 # Register your models here.
 admin.site.register(Post)
-
+admin.site.register(Comment)
+admin.site.register(Reviews)
+admin.site.register(Category)
--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,8 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,3 +17,50 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"This recipe {self.title} was created by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    parent = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE,
+        related_name="replies")
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["title"]
+    
+    def __str__(self):
+        return f"Category {self.title}"
+
+
+class Reviews(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="reviews")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviewer")
+    approved = models.BooleanField(default=False)
+    rating = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return f"{self.post}: {self.rating}"
