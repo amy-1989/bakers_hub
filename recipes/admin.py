8eca89c0e40e5290a5848976ebcd74fc8fe255fa
amy-1989 <>
2024-02-11 15:18:13 +0000

    update models

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 from .models import Post
 from .models import Comment
-from .models import Reviews
+from .models import Review
 from .models import Category
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Reviews)
+admin.site.register(Review)
 admin.site.register(Category)
--- recipes/models.py
+++ recipes/models.py
@@ -7,13 +7,25 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"Category {self.title}"
+
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='', null=True, related_name='categorys')
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     featured_image = CloudinaryField('image', default='placeholder')
+    ingredients = models.TextField(default='')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -44,23 +56,13 @@ class Comment(models.Model):
         return f"Comment {self.body} by {self.author}"
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-
-    class Meta:
-        ordering = ["title"]
-    
-    def __str__(self):
-        return f"Category {self.title}"
-
-
-class Reviews(models.Model):
+class Review(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="reviews")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="reviewer")
     approved = models.BooleanField(default=False)
-    rating = models.IntegerField(default = 0)
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.post}: {self.rating}"
