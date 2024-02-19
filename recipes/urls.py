21dd54c132a8309a891f6ce4d3762e7840cd5d3b
amy-1989 <amy1989moran@gmail.com>
2024-02-19 15:37:18 +0000

    add function and generic template to render individual recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.CategoryList.as_view(), name='home'),
     path("category/<category>/", views.recipe_category, name="recipe_category"),
+    path('<slug:slug>/', views.recipe_post, name='recipe_post')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,  get_list_or_404
+from django.shortcuts import render,  get_object_or_404
 from django.views import generic
 from .models import Post
 from .models import Category
@@ -19,4 +19,15 @@ def recipe_category(request, category):
         "posts": posts,
     }
     return render(request, "recipes/category.html", context)
+
+
+def recipe_post(request, slug):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "recipes/post.html",
+        {"post": post},
+    )
 
