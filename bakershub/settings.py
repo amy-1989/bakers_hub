04619a029cdd00a126616e5d5e15ab07d6a93194
amy-1989 <amy1989moran@gmail.com>
2024-02-22 20:21:03 +0000

    enable serving of static files

--- bakershub/settings.py
+++ bakershub/settings.py
@@ -61,8 +61,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'amy1989moran@gmail.com'
-EMAIL_HOST_PASSWORD = 'ezoy pqjg vnbf erzv'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 MIDDLEWARE = [
