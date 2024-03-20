3160c50f294dcb8ea4e29dc29a2121d8e9a9be03
amy-1989 <amy1989moran@gmail.com>
2024-03-20 22:57:24 +0000

    resize hero image

--- bakershub/settings.py
+++ bakershub/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8003-amy-1989-bakers-hub-qz29bz58xm.us2.codeanyapp.com',
                  '8000-amy1989-bakershub-z1qrot13eak.ws-eu110.gitpod.io',
