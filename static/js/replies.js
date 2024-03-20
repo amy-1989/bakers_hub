41634d71547f73f960c5f58214f78192b85aac17
amy-1989 <amy1989moran@gmail.com>
2024-03-20 11:45:06 +0000

    remove unnecessary js functions from comments and replies.js

--- static/js/replies.js
+++ static/js/replies.js
@@ -1,34 +1,6 @@
-const editReplyButtons = document.getElementsByClassName("btn-reply-edit");
-const replyText = document.getElementById("id_body");
-const replyForm = document.getElementById("replyForm");
-const submitReplyButton = document.getElementById("replyButton");
-
 const deleteReplyModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteReplyButtons = document.getElementsByClassName("btn-reply-delete");
 const deleteReplyConfirm = document.getElementById("deleteConfirm");
-
-/**
-* Initializes edit reply functionality for the provided edit reply buttons.
-* 
-* For each button in the `editReplyButtons` collection:
-* - Retrieves the associated reply's ID upon click.
-* - Fetches the content of the corresponding reply.
-* - Populates the `replyText` input/textarea with the reply's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_reply/{replyId}` endpoint.
-*/
-
-for (let button of editReplyButtons) {
-    button.addEventListener("click", (e) => {
-      let replyId = e.target.getAttribute("reply_id");
-      let replyContent = document.getElementById(`reply${replyId}`).innerText;
-      console.log(replyId);
-      console.log(replyContent);
-      replyText.value = replyContent;
-      submitReplyButton.innerText = "Update";
-      replyForm.setAttribute("action", `edit_reply/${replyId}`);
-    });
-  }
 
   /**
 * Initializes delete functionality for the provided delete reply buttons.
